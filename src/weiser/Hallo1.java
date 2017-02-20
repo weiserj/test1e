ba98f14cedf71ad2fede894bc2e304a6b30831cb
Johann Weiser <johann.weiser@aon.at>
2017-02-20 10:16:48 +0100

    neuer Brnach.

--- src/weiser/Hallo1.java
+++ src/weiser/Hallo1.java
@@ -4,6 +4,7 @@ public class Hallo1 {
 
 	public static void main(String[] args) {
 		System.out.println("Johann Weiser wünscht Dir einen guten Tag!");
+		System.out.println("in Branch \"testBranch1\"");
 	}
 
 }
